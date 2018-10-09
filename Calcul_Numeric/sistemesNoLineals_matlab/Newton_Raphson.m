a46e0a89af798b7a9cdf337710056188719f0cf9
Mikel <idrilirdi.tadziorobotics@gmail.com>
2018-10-09 18:09:00 +0200

    Finished code for Prac1

---
+++ Calcul_Numeric/sistemesNoLineals_matlab/Newton_Raphson.m
@@ -0,0 +1,33 @@
+function [x, r, err] = Newton_Raphson(x0, jacobiana, residu, F, niter, tol)
+
+J0 = jacobiana(F, x0);
+f0 = residu(F, x0); 
+iter = 0;
+Dx = zeros(length(x0), 1);
+x1 = zeros(length(x0), 1);
+err = [norm(f0)];
+
+fprintf('INICIO NEWTON_RAPHSON \n')
+
+while (iter < niter) && (norm(f0) > tol)
+    %fprintf('iter = %d, error = %e \n',iter,err(iter+1))
+    %x0
+    Dx = J0\-f0;
+    x1 = x0 + Dx;
+    
+    J0 = jacobiana(F, x1); f0 = residu(F, x1); x0 = x1;
+    err = [err, norm(f0)];
+    iter = iter + 1;
+end
+if (iter < niter)
+fprintf('Hacen falta %d iteraciones para llegar al error %e con NEWTON_RAPHSON \n',iter,err(iter-1));
+else
+    fprintf('Hemos hecho %d iteraciones y no ha convergido, error final %e \n', niter, err(iter-1))
+end
+
+
+x = x1; 
+r = f0;
+
+end
+

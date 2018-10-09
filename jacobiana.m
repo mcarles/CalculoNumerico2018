dbcc9f6444b76c31d1ac61c97b170e8cfa8553bf
Idril <idrilirdi.tadziorobotics@gmail.com>
2018-10-09 16:09:13 +0200

    cleanup

--- jacobiana.m
+++
@@ -1,6 +0,0 @@
-function [res] = jacobiana(x)
-res = [6, 2*sin(x(2)*x(3))*x(3), 2*sin(x(2)*x(3))*x(2);
-x(1)/(sqrt(x(1).^2 + sin(x(3)) + 1.06)), 9, cos(x(3))/(2*sqrt(x(1).^2 + sin(x(3)) + 1.06));
-3*x(2)*exp(x(2)*x(1)), 3*x(1)*exp(x(2)*x(1)), 60];
-end
-

000621bb3f16054d99b2a874c376318f0395b3fd
mcarles <mikel.carles@gmail.com>
2018-10-16 12:34:35 +0200

    initial upload splines

---
+++ Calcul_Numeric/Fitxers matlab-20181016/calculaCurvaturesSplineNatural.m
@@ -0,0 +1,25 @@
+function d2S = calculaCurvaturesSplineNatural(x,y)
+d2s = [];
+h=x(2:end)-x(1:end-1);
+t=y(2:end)-y(1:end-1);
+la = h(2:end)/(h(2:end) - h(1:end-1)); %lambda
+mu = h(1:end-1)/(h(2:end) - h(1:end-1)); %mu
+d = 6/(h(2:end) - h(1:end-1))*(t(2:end)/h(2:end) - t(1:end-1)/h(1:end-1));
+
+n = length(x)-1;
+M = zeros(n-1);
+M(1,1) = 2;
+M(1,2) = la(1);
+M(n-2,n-1) = mu(n-1);
+M(n-1,n-1) = 2;
+for i=2:n-2
+   M(i,i) = 2;
+   M(i,i-1) = mu(i);
+   M(i,i+1) = la(i);
+end
+
+d2s = M\d;
+
+d2s = [0; d2s; 0];
+end
+

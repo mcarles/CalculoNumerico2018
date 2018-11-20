5e48911a576450eca78ea716a5312f1204f7da6f
mikel <mikel.carles@gmail.com>
2018-11-20 17:27:15 +0100

    Preemtive save

--- Calcul_Numeric/Integracio/Entregable IintNum/Test.m
+++ Calcul_Numeric/Integracio/Entregable IintNum/Test.m
@@ -1,9 +1,8 @@
-function [S, nint, x] = IntAdapt(f,a,b,tol,n, pts)
+function [S, x] = Test(f,a,b,tol,pts)
     %Calculo de las Ss del enunciado usando Simpson Simple.
     S1 = SimpsonSimple(f,a,b);
     S2 = SimpsonSimple(f,a,(a+b)/2);
     S3 = SimpsonSimple(f,(a+b)/2,b);
-    nint = n;
     x = pts;
     %Calculo del error
     Eab = abs(S1 - (S2 + S3));
@@ -14,17 +13,9 @@ function [S, nint, x] = IntAdapt(f,a,b,tol,n, pts)
        x = [pts, a, b];
        return;
     else
-       %Llamada recursiva, como metes 2 intervalos nuevos por llamada se
-       %suma 2 al numero de intervalos
-       nint = nint + 1;
-       
-       [Sa, ninta, xa] = IntAdapt(f, (a+b)/2, b, tol, nint, pts);
-       [Sb, nintb, xb] = IntAdapt(f, a, (a+b)/2, tol, nint, pts);
+       [Sa, xa] = Test(f, (a+b)/2, b, tol, pts);
+       [Sb, xb] = Test(f, a, (a+b)/2, tol, pts);
        x = [xa, xb];
-       
        S = Sa + Sb;
-       nint = max(ninta,nintb);
     end
-
 end
-

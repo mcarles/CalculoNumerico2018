b258c9d64ca47d06bc72c9dd1c4c65ae3f4662df
mikel <mikel.carles@gmail.com>
2018-11-20 12:29:46 +0100

    CalNum Homework pt2

--- Calcul_Numeric/Integracio/Entregable IintNum/IntAdapt.m
+++ Calcul_Numeric/Integracio/Entregable IintNum/IntAdapt.m
@@ -1,22 +1,29 @@
-function [S,nint] = IntAdapt(f,a,b,tol,n)
+function [S, nint, x] = IntAdapt(f,a,b,tol,n, pts)
     %Calculo de las Ss del enunciado usando Simpson Simple.
     S1 = SimpsonSimple(f,a,b);
     S2 = SimpsonSimple(f,a,(a+b)/2);
     S3 = SimpsonSimple(f,(a+b)/2,b);
-    
+    nint = n;
+    x = pts;
     %Calculo del error
     Eab = abs(S1 - (S2 + S3));
+    
     if (Eab < tol*(b-a))
        %Si has aproximado bien, acaba y devuelve
        S = S1;
-       nint = n;
+       x = [pts, a, b]
        return;
     else
        %Llamada recursiva, como metes 2 intervalos nuevos por llamada se
        %suma 2 al numero de intervalos
-       n = n + 2;
-       nint = n;
-       S = IntAdapt(f,(a+b)/2,b,tol,n) + IntAdapt(f,a,(a+b)/2,tol,n);
+       nint = nint + 1;
+       
+       [Sa, ninta, xa] = IntAdapt(f, (a+b)/2, b, tol, nint, pts);
+       [Sb, nintb, xb] = IntAdapt(f, a, (a+b)/2, tol, nint, pts);
+       
+       S = Sa + Sb;
+       nint = max(ninta,nintb);
     end
+
 end
 
--- Calcul_Numeric/Integracio/Entregable IintNum/main.m
+++ Calcul_Numeric/Integracio/Entregable IintNum/main.m
@@ -1,7 +1,11 @@
+clear; close all; clc
 g = @(x) sin(exp(2*x));
 f = @(x) x;
 a = 0;
 b = 2;
 tol = 10.^-3;
 Ie = integral(g,a,b)
-[Ia,nint] = IntAdapt(g,a,b,tol,0)
+pts =[a,b];
+[Ia,nint, x] = IntAdapt(g,a,b,tol,1, pts);
+x = sort(x);
+x = unique(x);
